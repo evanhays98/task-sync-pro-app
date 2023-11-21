8b87ca1d4f237911e8768f71f5dc9b7a931a02f9
evanhays98 <evan.hays@epita.fr>
2023-11-21 19:32:15 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,169 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Monoton&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+#app {
+  font-family: "Montserrat", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  position: relative;
+  min-height: 100%;
+  height: 100vh;
+  overscroll-behavior: none;
+  overflow: hidden;
+}
+
+html {
+  box-sizing: border-box;
+  height: 100%;
+  overflow: hidden;
+  overscroll-behavior: none;
+}
+
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+input:-webkit-autofill:active {
+  transition: background-color 5000s ease-in-out 0s;
+  -webkit-text-fill-color: #e2e2d5 !important;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body {
+  box-sizing: border-box;
+  margin: 0;
+  border: 0;
+  background: #131616;
+  overflow: hidden;
+  padding: 0 0 0 calc(100vw - 100%);
+  width: 100%;
+  height: 100%;
+  min-height: 100%;
+  font-family: $Montserrat;
+  overscroll-behavior: none;
+
+  * {
+    text-decoration: none;
+    list-style: none;
+  }
+
+  *:visited {
+    text-decoration: none;
+  }
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font: inherit;
+  vertical-align: baseline;
+}
+
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="background-container">
+    <h1>TaskSync PRO</h1>
+    <h2>Sync task with ease, enpower your productivity</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class HelloWorld extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Monoton&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+h1 {
+  color: aliceblue;
+  font-size: 4rem;
+  font-weight: 900;
+  font-family: "Montserrat", sans-serif;
+  text-align: left;
+  padding: 20px;
+}
+h2 {
+  font-size: 2.5rem;
+  font-weight: 800;
+  position: absolute;
+  bottom: 20px;
+  padding: 20px;
+  border-radius: 10px;
+  -webkit-background-clip: text;
+  color: #e0eeeb;
+  width: fit-content;
+  max-width: 50%;
+  right: 10px;
+  backdrop-filter: blur(4px);
+  text-align: center;
+}
+.background-container {
+  background-image: url("../assets/taskSyncMonkey.png");
+  background-size: cover; /* Adjust this to your preference */
+  background-position: center; /* Adjust this to your preference */
+  background-repeat: no-repeat; /* Adjust this to your preference */
+  width: 100%;
+  height: 100vh;
+  position: relative;
+}
+</style>
